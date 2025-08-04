5df9a8b424a7647aebcda2ecef495f3e88b35d48
phongvt-remvn <phongvo.remvn41@gmail.com>
2025-08-04 13:54:11 +0700

    feat: create base layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">© 2025 REM Vietnam, Inc</p>
+
+    <p class="col-md-4 mb-0 text-muted text-end">PhongVT</p>
+</div>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;" data-bs-theme="light">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="36" height="30">
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarColor03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/books">Books</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="">Authors</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Blogs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Contact</a>
+        </li>
+      </ul>
+
+      <button class="btn btn-outline-primary" type="submit" disabled>Account</button>
+    </div>
+  </div>
+</nav>
